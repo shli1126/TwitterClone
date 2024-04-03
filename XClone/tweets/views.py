e465575b040ffb5eb5aa48a140d292393ea88fa9
Shaolong Li <shaolongli26@gmail.com>
2024-04-03 16:57:30 -0700

    add test model and test view

--- XClone/tweets/views.py
+++ XClone/tweets/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import Tweet
 
-# Create your views here.
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    return HttpResponse("<h1>Hello World</h1>")
+
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    data = {
+        "id": tweet_id,
+        "content": ""
+    }
+    status= 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not found"
+        status = 404
+    
+    return JsonResponse(data, status=status)
