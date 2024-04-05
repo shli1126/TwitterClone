c0bc99946e9e7c7c227ef356b8f2f9f5968f1d52
shaolongli <shaolongli26@gmail.com>
2024-04-04 17:09:05 -0700

    add get tweets and create tweets api

--- XClone/tweets/views.py
+++ XClone/tweets/views.py
@@ -1,24 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Tweet
+import random
+from .forms import TweetForm
+
 
 def home_view(request, *args, **kwargs):
-    print(args, kwargs)
-    return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "pages/home.html", context={}, status=200)
+
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [
+        {"id": x.id, "content": x.content, "likes": random.randint(1, 100)} for x in qs
+    ]
+    data = {"isUser": False, "response": tweets_list}
+    return JsonResponse(data)
+
+
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        form = TweetForm()
+    return render(request, "components/form.html", context={"form": form})
 
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    data = {
-        "id": tweet_id,
-        "content": ""
-    }
-    
-    status= 200
+    data = {"id": tweet_id, "content": ""}
+
+    status = 200
     try:
         obj = Tweet.objects.get(id=tweet_id)
-        data['content'] = obj.content
+        data["content"] = obj.content
     except:
-        data['message'] = "Not found"
+        data["message"] = "Not found"
         status = 404
-    
+
     return JsonResponse(data, status=status)
